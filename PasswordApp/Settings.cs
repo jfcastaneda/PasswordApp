c218a8d8c63dd66541e9fe51e92348deb8c8d04f
Jose Castaneda <jfcastanedajr@gmail.com>
2014-03-11 21:07:20 -0500

    Initial Commit
    Setting up assingment

---
+++ PasswordApp/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+
+namespace PasswordApp
+{
+    public static class Settings
+    {
+        public static System.Collections.ObjectModel.ObservableCollection<Password> PasswordsList
+        {
+
+        }
+
+        public int CurrentIndex
+        {
+
+        }
+
+        public IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+    }
+}
